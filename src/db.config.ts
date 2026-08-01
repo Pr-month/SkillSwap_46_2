dd2cc9346e38bde3cd44a6c611872c208d65778d
Maria Eliseeva <marymaryeliseeva@yandex.ru>
2026-08-02 02:40:07 +0300

    fix(db): исправлены пути

--- src/db.config.ts
+++ src/db.config.ts
@@ -8,8 +8,8 @@ export const dbConfig = registerAs('DB_CONFIG', () => ({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
 } as DataSourceOptions));
 
