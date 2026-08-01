f15d582b36f6003237afaaeaac8077d1951feba6
Maria Eliseeva <marymaryeliseeva@yandex.ru>
2026-08-01 15:42:45 +0300

    feat(db): добавлен конфиг бд, пакеты, подключение в app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { dbConfig } from './db.config';
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
