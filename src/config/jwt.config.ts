bca6a95cab6df81dbe3426cd6f0c73fb539d65da
ReshetnikovAV89 <andrey.reshetnikov.89@mail.ru>
2026-08-02 16:55:23 +0800

    feat(config): add JWT configuration

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,8 @@
+import { registerAs } from '@nestjs/config';
+
+export const jwtConfig = registerAs('jwt', () => ({
+  accessSecret: process.env.JWT_ACCESS_SECRET ?? '',
+  refreshSecret: process.env.JWT_REFRESH_SECRET ?? '',
+  accessExpiresIn: Number(process.env.JWT_ACCESS_EXPIRES_IN ?? 900),
+  refreshExpiresIn: Number(process.env.JWT_REFRESH_EXPIRES_IN ?? 604800),
+}));
