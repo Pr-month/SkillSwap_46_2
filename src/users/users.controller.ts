34efa824835fa15aa75ec1577f652b5c3a9fe26f
Ivan Aleks <Tolpil@yandex.ru>
2026-08-02 21:21:30 +0200

    feat(users): реализовал эндпоинт GET users/me

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AccessTokenGuard } from '../auth/guards/accessToken.guard';
+import { JwtPayload } from '../auth/auth.types';
 
 @Controller('users')
 export class UsersController {
@@ -23,6 +28,13 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Get('me')
+  @UseGuards(AccessTokenGuard)
+  getMe(@Req() req: Request) {
+    const user = req.user as JwtPayload;
+    return this.usersService.findById(user.sub);
   }
 
   @Get(':id')
