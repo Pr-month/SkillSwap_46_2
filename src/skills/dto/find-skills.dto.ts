3f9b201f2973c366f3827bd6ed27ed4c21ea9505
Mikhail Kozkin <Mikhail.Kozkin@yandex.kz>
2026-08-03 16:44:02 -0700

    feat(skills): реализован GET /skills с пагинацией, поиском и валидацией query-параметров

---
+++ src/skills/dto/find-skills.dto.ts
@@ -0,0 +1,27 @@
+import { Type } from 'class-transformer';
+import { 
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Max } from 'class-validator';
+
+  export class FindSkillsDto {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @IsPositive()
+    page: number = 1;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @IsPositive()
+    @Max(50)
+    limit: number = 20;
+
+    @IsOptional()
+    @IsString()
+    search: string = '';
+
+}
--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Skill } from './entities/skill.entity';
+import { FindSkillsDto } from './dto/find-skills.dto';
 import { CreateSkillDto } from './dto/create-skill.dto';
 import { UpdateSkillDto } from './dto/update-skill.dto';
 
 @Injectable()
 export class SkillsService {
+  constructor(
+    @InjectRepository(Skill)
+    private readonly skillsRepository: Repository<Skill>,
+  ) {}
+
   create(createSkillDto: CreateSkillDto) {
     return 'This action adds a new skill';
   }
 
-  findAll() {
-    return `This action returns all skills`;
+  async findAll(dto: FindSkillsDto) {
+    const { page, limit, search } = dto;
+
+    const query = this.skillsRepository
+      .createQueryBuilder('skill')
+      .where('LOWER(skill.title) LIKE LOWER(:search)', {
+        search: `%${search}%`,
+      })
+      .skip((page - 1) * limit)
+      .take(limit);
+
+    const [data, total] = await query.getManyAndCount();
+
+    return {
+      data,
+      page,
+      totalPages: Math.ceil(total / limit),
+    };
   }
 
   findOne(id: number) {
