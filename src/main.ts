45c484a458bf2d692c9669892ffc4852e32dac81
ReshetnikovAV89 <andrey.reshetnikov.89@mail.ru>
2026-08-05 11:11:38 +0800

    refactor(config): use app config port

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import type { ConfigType } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
+import { appConfig } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const application = await NestFactory.create(AppModule);
+  const applicationConfiguration = application.get<
+    ConfigType<typeof appConfig>
+  >(appConfig.KEY);
 
-  app.useGlobalFilters(new AllExceptionFilter());
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  application.useGlobalFilters(new AllExceptionFilter());
+  application.useGlobalInterceptors(
+    new ClassSerializerInterceptor(application.get(Reflector)),
+  );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await application.listen(applicationConfiguration.port);
 }
 
 void bootstrap();
