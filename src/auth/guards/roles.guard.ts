ba56c5d365b46d8b046be6a9e81b0f36c12da67f
Ekaterina Semenova <ek.semenova94@gmail.com>
2026-08-06 15:30:42 +0300

    fix: исправлены ошибки линтера, типизации

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,11 +1,8 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Roles } from '../decorators/roles.decorator';
 import { Role } from 'src/shared/enums/role.enum';
+import { RequestWithUser } from '../auth.types';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -21,7 +18,7 @@ export class RolesGuard implements CanActivate {
       return true;
     }
 
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
     const user = request.user;
 
     if (!user?.role) {
