e5f9aa71c429c58947c2394082b857d3661c669a
Ekaterina Semenova <ek.semenova94@gmail.com>
2026-08-07 19:01:27 +0300

    feat: добавлен CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,40 @@
-export class CreateUserDto {}
+import {
+  IsDate,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Gender } from 'src/shared/enums/gender.enum';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  // @Matches('^(?=.*[A-Za-z])(?=.*\d)(?=.*[!"#$%&'()*+,\-./:;<=>?@[\]^_`{|}~])[\S]+$')
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  about: string;
+
+  @IsOptional()
+  @IsDate()
+  birthdate: Date;
+
+  @IsOptional()
+  @IsString()
+  city: string;
+
+  @IsOptional()
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @IsOptional()
+  @IsString()
+  avatar: string;
+}
