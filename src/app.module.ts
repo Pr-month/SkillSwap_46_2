d837784cd8c8a5e59765dee38cbdb887bea15650
Evgeny Razorenov <dumpil5@gmail.com>
2026-08-12 10:39:36 +0300

    feat(cities): генерация ресурса Cities (#85)

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { SkillsModule } from './skills/skills.module';
 import { CategoriesModule } from './categories/categories.module';
 import { FilesModule } from './files/files.module';
 import { User } from './users/entities/user.entity';
+import { CitiesModule } from './cities/cities.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { User } from './users/entities/user.entity';
     SkillsModule,
     CategoriesModule,
     FilesModule,
+    CitiesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
