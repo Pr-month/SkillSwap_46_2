bcb0605660499a898cf389b9a0e8fe59d4661646
poplovetsl <leha825165@mail.ru>
2026-08-19 18:51:01 +0300

    feat(cities): удаление города по эндпоинту DELETE cities

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,6 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CitiesService } from './cities.service';
 import { CityShort } from './cities.types';
+import { AccessTokenGuard } from 'src/auth/guards/accessToken.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Role } from 'src/shared/enums/role.enum';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('cities')
 export class CitiesController {
@@ -9,5 +23,13 @@ export class CitiesController {
   @Get()
   async findAll(@Query('search') search?: string): Promise<CityShort[]> {
     return this.citiesService.search(search);
+  }
+
+  @Delete(':id')
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  @Roles([Role.ADMIN])
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.citiesService.remove(id);
   }
 }
--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Repository, ILike } from 'typeorm';
 import { City } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -20,5 +24,24 @@ export class CitiesService {
       order: { name: 'ASC' },
       take: SEARCH_RESULTS_LIMIT,
     });
+  }
+
+  async remove(id: string): Promise<void> {
+    const city = await this.cityRepository.findOne({
+      where: { id },
+      relations: { users: true },
+    });
+
+    if (!city) {
+      throw new NotFoundException(`Город с id "${id}" не найден`);
+    }
+
+    if (city.users.length > 0) {
+      throw new BadRequestException(
+        'Нельзя удалить город, к которому привязаны пользователи',
+      );
+    }
+
+    await this.cityRepository.delete(id);
   }
 }
