af2426896381461f83a141e695ee926d64d5a445
Mikhail Kozkin <Mikhail.Kozkin@yandex.kz>
2026-09-08 10:30:00 -0700

    fix(static и helmet): раздача файлов резолвится через process.cwd() вместо __dirname и разрешить загрузку изображений с бэкенда через Helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,9 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(helmet());
+  app.use(helmet({
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+  }));
   app.setGlobalPrefix('api');
   const applicationConfiguration = app.get<ConfigType<typeof appConfig>>(
     appConfig.KEY,
