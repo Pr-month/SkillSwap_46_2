1e973715da98c557a481e7e98ec239cc41c636c8
Ivan Aleks <Tolpil@yandex.ru>
2026-09-09 16:42:09 +0500

    feat(mail): отправка писем на электронную почту

--- backend/src/requests/requests.service.spec.ts
+++ backend/src/requests/requests.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RequestsService } from './requests.service';
+import { MailService } from '../mail/mail.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Request } from './entities/request.entity';
+import { Skill } from '../skills/entities/skill.entity';
 
 describe('RequestsService', () => {
   let service: RequestsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RequestsService],
+      providers: [
+        RequestsService,
+        {
+          provide: MailService,
+          useValue: { sendUserNotification: jest.fn() },
+        },
+        {
+          provide: getRepositoryToken(Request),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Skill),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<RequestsService>(RequestsService);
